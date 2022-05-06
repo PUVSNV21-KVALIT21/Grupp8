afe73c6922c0d434d36df1a09d345c937a755369
sara-levin <sara.levin@studerande.plushogskolan.se>
2022-05-06 11:40:13 +0200

    Category CRUD

---
+++ HakimsLivs/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HakimsLivs.Data;
+using HakimsLivs.Models;
+
+namespace HakimsLivs.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly HakimsLivs.Data.ApplicationDbContext _context;
+
+        public DeleteModel(HakimsLivs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Categories.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Categories.FindAsync(id);
+
+            if (Category != null)
+            {
+                _context.Categories.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
