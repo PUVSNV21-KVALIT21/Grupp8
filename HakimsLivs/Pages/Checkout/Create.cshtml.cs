bfb02764083ba2d3c79df9556846ede3cdaf3621
Teobl <Teo.Blazevic@hotmail.com>
2022-05-13 11:39:17 +0200

    Receipt

---
+++ HakimsLivs/Pages/Checkout/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HakimsLivs.Data;
+using HakimsLivs.Models;
+
+namespace HakimsLivs.Pages.Checkout
+{
+    public class CreateModel : PageModel
+    {
+        private readonly HakimsLivs.Data.ApplicationDbContext _context;
+
+        public CreateModel(HakimsLivs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public OrderProduct OrderProduct { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.OrderProducts.Add(OrderProduct);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ HakimsLivs/Pages/Checkout/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HakimsLivs.Data;
+using HakimsLivs.Models;
+
+namespace HakimsLivs.Pages.Checkout
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly HakimsLivs.Data.ApplicationDbContext _context;
+
+        public DetailsModel(HakimsLivs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public OrderProduct OrderProduct { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderProduct = await _context.OrderProducts.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (OrderProduct == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
