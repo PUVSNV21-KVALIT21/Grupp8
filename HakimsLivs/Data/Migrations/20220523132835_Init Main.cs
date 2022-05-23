5a2def1aee5463d71b8e96fd310b5b245cf06463
sara-levin <sara.levin@studerande.plushogskolan.se>
2022-05-23 15:35:24 +0200

    Final Project Upload

---
+++ HakimsLivs/Data/Migrations/20220523132835_Init Main.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HakimsLivs.Data.Migrations
+{
+    public partial class InitMain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
